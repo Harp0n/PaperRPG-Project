cbf2edb769ecc9713ad473b32fdf2eef30a20119
mihal09 <mjanik1@op.pl>
2019-11-09 00:32:13 +0100

    nie dziala

--- test.py
+++ test.py
@@ -37,10 +37,63 @@ def addTables():
                         cursor.execute(dropQuery)
                 
                 cursor.execute(final_query)
-                
-addTables()
 
     
+def generateData():
+    with open("data/5e-SRD-Races.json", "r") as read_file:
+        queryRaces = "INSERT INTO races VALUES (NULL,%s,%s,%s,%s,%s,%s,%s,%s);"
+        queryRaceAbilities = "INSERT INTO raceAbilities VALUES (NULL,%s,%s,%s,%s);"
+        queryRaceStartingProficiency = "INSERT INTO starting_proficiences VALUES (NULL,%s,%s,%s);"
+        data = json.load(read_file)
+        for race in data:
+            _idRace = race['index']
+            _name = race['name']
+            _speed = race['speed']
+            _alignment = race['alignment']
+            _age = race['age']
+            _size = race['size']
+            _size_description = race['size_description']
+            if "choose" in  race['ability_bonus_options']:
+                _ability_bonus_options_count = race['ability_bonus_options']['choose']
+            else:
+                _ability_bonus_options_count = 0
+            if "choose" in  race['starting_proficiency_options']:
+            if(tmp != ""):
+                _starting_proficiences_options_count = race['starting_proficiency_options']['choose']
+            else:
+                _starting_proficiences_options_count = 0
+            parameters = (_name, _speed, _alignment, _age, _size, _size_description, _ability_bonus_options_count, _starting_proficiences_options_count)
+            cursor.execute(queryRaces,parameters)
+            
+            for ability_bonus in race['ability_bonuses']:
+                _bonus = ability_bonus['bonus']
+                _idAbility = ability_bonus['url'].split("/")[-1]
+                _isOptional = False
+                parameters = (_bonus, _idRace, _idAbility, _isOptional)
+                cursor.execute(queryRaceAbilities, parameters)
+                
+            for ability_bonus in race['ability_bonus_options']:
+                _bonus = ability_bonus['bonus']
+                _idAbility = ability_bonus['url'].split("/")[-1]
+                _isOptional = True
+                parameters = (_bonus, _idRace, _idAbility, _isOptional)
+                cursor.execute(queryRaceAbilities, parameters)
+                
+            for starting_proficiency in race['starting_proficiencies']:
+                _isOptional = False
+                _idProficiency = starting_proficiency['url'].split("/")[-1]
+                parameters = (_isOptional, _idRace, _idProficiency)
+                cursor.execute(queryRaceStartingProficiency, parameters)
+                
+            for starting_proficiency in race['starting_proficiency_options']['from']:
+                _isOptional = True
+                _idProficiency = starting_proficiency['url'].split("/")[-1]
+                parameters = (_isOptional, _idRace, _idProficiency)
+                cursor.execute(queryRaceStartingProficiency, parameters)
+
+            
+addTables()
+generateData()    
 
 
 
