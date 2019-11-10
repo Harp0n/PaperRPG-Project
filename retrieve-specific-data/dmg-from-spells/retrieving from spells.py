f1a7d921f3e88c15efc662c0c86fc09017c0186a
Radosław Karbowiak <41492536+Reevo55@users.noreply.github.com>
2019-11-10 12:19:53 +0100

    Retrieving dmg type (not complete)
    Partialy done but sometimes just doesn't work, dunno why. And also i need to make diffrence between dmg spells and healing spells.

--- retrieve-specific-data/dmg-from-spells/retrieving from spells.py
+++ retrieve-specific-data/dmg-from-spells/retrieving from spells.py
@@ -11,7 +11,14 @@ def retriveDamage(spellDescription):
             if hasNumbers(word):
                 if word.find("d") == 1:
                     return word
-
+def retriveDmgType(spellDescription, dmgTypes):
+    spellDesc = spellDescription.split()
+    for desc in spellDesc:
+        for dmgType in dmgTypes:
+            if dmgType.lower() in spellDesc:
+                return dmgType
+        
+    
 
 file = open("Spells.json", "r") 
 
@@ -19,22 +26,41 @@ parseJson = json.load(file)
 
 listOfDmgSpells = []
 
+fileDmgTypes = open("5e-SRD-Damage-Types.json", "r")
+parsedDMGtypes = json.load(fileDmgTypes)
+
+stringTest = "4d4 damage acid"
+
+print("acid" in stringTest.split())
+
+dmgTypes = []
+      
+for dmgType in parsedDMGtypes:
+    dmgTypes.append(dmgType["name"])
+    ##print(dmgType["name"])
+
+
 for spell in parseJson:
     description = spell["desc"]
     for desc in description:
         dmg = retriveDamage(desc)
+        dmgType = retriveDmgType(desc, dmgTypes)
 
-    if dmg != None:
-        print(spell["name"] + " " + dmg)
+    if dmg != None and dmgType != None:
+        helpString = (spell["name"] + " " + dmg + " DMG TYPE: " + dmgType)
+        listOfDmgSpells.append(helpString)
+
+    elif dmg != None:
         helpString = (spell["name"] + " " + dmg)
         listOfDmgSpells.append(helpString)
     else:
-        print(spell["name"] + " no dmg")
         helpString = (spell["name"] + " no dmg")
         listOfDmgSpells.append(helpString)
+        
 
         
-print(len(listOfDmgSpells))
+for spell in listOfDmgSpells:
+    print(spell)
 
 
 
