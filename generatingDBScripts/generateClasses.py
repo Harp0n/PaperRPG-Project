1b041e7ffa164c5ce65ef2155fdbc5da2a2e6264
mihal09 <mjanik1@op.pl>
2019-11-11 23:59:38 +0100

    .

---
+++ generatingDBScripts/generateClasses.py
@@ -0,0 +1,68 @@
+from connectDB import db, cursor, saveDB, closeDB
+import json
+
+def generate():
+    with open("data/55e-SRD-Classes.json", "r") as read_file:
+            queryRaces = "INSERT INTO classes VALUES (NULL,%s,%s,%s,%s,%s,%s,%s,%s);"
+            data = json.load(read_file)
+            for c in data:
+                _name = c['name']
+                _hitDie = c['hit_die']
+                if('choose' in c['proficiency_choices'])
+                    _proficiencyChoicesCount = c['proficiency_choices']['chose']
+
+
+                if('from' in c['proficiency_choices']):
+                    for proficiency in c['proficiencies']:
+
+
+                for proficiency in c['proficiencies']:
+
+                
+
+
+
+            parameters = (_type, _name)
+            cursor.execute(query,parameters)
+
+
+
+
+
+
+
+
+
+                for ability_bonus in race['ability_bonuses']:
+                    _bonus = ability_bonus['bonus']
+                    _idAbility = ability_bonus['url'].split("/")[-1]
+                    _isOptional = False
+                    parameters = (_bonus, _idRace, _idAbility, _isOptional)
+                    cursor.execute(queryRaceAbilities, parameters)
+
+                if('from' in race['ability_bonus_options']):
+                    for ability_bonus in race['ability_bonus_options']['from']:
+                        _bonus = ability_bonus['bonus']
+                        _idAbility = ability_bonus['url'].split("/")[-1]
+                        _isOptional = True
+                        parameters = (_bonus, _idRace, _idAbility, _isOptional)
+                        cursor.execute(queryRaceAbilities, parameters)
+
+                    
+                for starting_proficiency in race['starting_proficiencies']:
+                    _isOptional = False
+                    _idProficiency = starting_proficiency['url'].split("/")[-1]
+                    parameters = (_isOptional, _idRace, _idProficiency)
+                    cursor.execute(queryRaceStartingProficiency, parameters)
+
+                if('from' in race['starting_proficiency_options']):
+                    for starting_proficiency in race['starting_proficiency_options']['from']:
+                        _isOptional = True
+                        _idProficiency = starting_proficiency['url'].split("/")[-1]
+                        parameters = (_isOptional, _idRace, _idProficiency)
+                        cursor.execute(queryRaceStartingProficiency, parameters)
+
+if __name__ == "__main__":
+    generate()
+    saveDB()
+    closeDB()
